5dea413d1ba9e6a18ee97170ef913a08a05aeadf
William Stevens <wstevens.hornet@gmail.com>
2019-10-27 06:21:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,16 @@
 class Artist 
   
-  attr_accessor: :song 
+  attr_accessor: :song, :name
   
-  def initialize(song) 
-    
+  def initialize(song, name) 
+    @song = song 
+    @name = name 
+  end
+  
+  @@number_songs = 0 
+  
+  def add_song_by_name(name) 
+    @song << name
   end
   
 end
